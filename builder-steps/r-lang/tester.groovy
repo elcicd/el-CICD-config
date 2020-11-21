76463083343fab6397ec246e8cc9ab6219aae1fb
hippyod <hippyod@yahoo.com>
2020-11-20 22:24:10 -0600

    first pass moving files to el-CICD or el-CICD-project-repository

--- builder-steps/r-lang/tester.groovy
+++ builder-steps/r-lang/tester.groovy
@@ -5,18 +5,13 @@
  */
 
 def test(def projectId, def microService) {
-    writeFile file:"${el.cicd.BUILDER_STEPS_DIR}/Rtest.R",
-                text: libraryResource("builder-steps/${microService.codeBase}/resources/Rtest.R")
-    writeFile file:"${el.cicd.BUILDER_STEPS_DIR}/Rlint.R",
-                text: libraryResource("builder-steps/${microService.codeBase}/resources/Rlint.R")
-
     sh """
-        Rscript ${el.cicd.BUILDER_STEPS_DIR}/Rlint.R
+        Rscript ${el.cicd.BUILDER_STEPS_DIR}/${microService.codeBase}/resources/Rlint.R
         mkdir -p ./tests
         if [[ `ls -l ./tests/*.R | wc -l` -gt 0 ]]
         then
             echo "Found tests available to execute"
-            Rscript ${el.cicd.BUILDER_STEPS_DIR}/Rtest.R
+            Rscript ${el.cicd.BUILDER_STEPS_DIR}/${microService.codeBase}/resources/Rtest.R
             badString='path="/'
             goodString='path="./'
             cat bad-coverage.xml | sed "s|\${badString}|\${goodString}|g" > coverage.xml
