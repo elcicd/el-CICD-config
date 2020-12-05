2058557d4d1f67be2dfddc2e3b76d2a3e141dc88
hippyod <hippyod@yahoo.com>
2020-12-04 18:05:56 -0600

    testing

--- hookScripts/on-fail-non-prod-project-onboarding.groovy
+++ hookScripts/on-fail-non-prod-project-onboarding.groovy
@@ -2,7 +2,7 @@
 def call(def exception) {
     echo exception
 
-    pipelineUtils.choBanner("WE'RE IN THE FAIL SCRIPT: ${exception?.message} !!!!!!")
+    pipelineUtils.choBanner("WE'RE IN THE FAIL SCRIPT: ${exception?.getMessage()} !!!!!!")
 }
 
 return this
