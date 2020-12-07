5a5c334260ffbea4dcfaec37a3a3aeeec8d390eb
hippyod <hippyod@yahoo.com>
2020-12-07 11:04:35 -0600

    renaming hookScripts to hook-scripts to follow convention; minor changes

--- hook-scripts/on-fail-non-prod-project-onboarding.groovy
+++ hook-scripts/on-fail-non-prod-project-onboarding.groovy
@@ -1,5 +1,5 @@
 
-def call(def exception) {
+def call(Map args, def exception) {
     pipelineUtils.echoBanner("WE'RE IN THE FAIL SCRIPT: ${exception?.getMessage()} !!!!!!")
 }
 
--- hook-scripts/pre-microservice-promotion-removal.groovy
+++ hook-scripts/pre-microservice-promotion-removal.groovy
@@ -1,6 +1,6 @@
 
 
-def call() {
+def call(Map args) {
     pipelineUtils.echoBanner("WE'RE IN THE PRE-HOOK SCRIPT!!!!!!")
 }
 return this
