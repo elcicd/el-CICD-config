eda19aa3bc49c4d812d5ae86864aa9903d6ede5a
hippyod <hippyod@yahoo.com>
2020-12-09 13:02:24 -0600

    programming error

--- hook-scripts/pre-non-prod-project-onboarding.groovy
+++ hook-scripts/pre-non-prod-project-onboarding.groovy
@@ -3,21 +3,20 @@
  */
 
 def call(Map args) {
-    def projectInfo = args.projectInfo
     dir (el.cicd.PROJECT_DEFS_DIR) {
-        sh "cp ${projectInfo.id}.yml ${projectInfo.id}-test.yml"
+        sh "cp ${args.projectId}.yml ${args.projectId}-test.yml"
 
         withCredentials([sshUserPrivateKey(credentialsId: el.cicd.EL_CICD_PROJECT_INFO_REPOSITORY_READ_ONLY_GITHUB_PRIVATE_KEY_ID, keyFileVariable: 'GITHUB_PRIVATE_KEY')]) {
             sh """
-                #git add ${el.cicd.PROJECT_DEFS_DIR}/${projectInfo.id}.yml
+                #git add ${el.cicd.PROJECT_DEFS_DIR}/${args.projectId}.yml
                 #git config user.name "el-CICD"
                 #git config user.email "${el.cicd.DEVOPS_EMAIL_LIST}"
-                #git commit -am 'writing el-CICD-config Project Definition File for ${projectInfo.id}'
+                #git commit -am 'writing el-CICD-config Project Definition File for ${args.projectId}'
                 ${sshAgentBash GITHUB_PRIVATE_KEY,
                                "git config --global user.name \"el-CICD\"",
                                "git config --global user.email \"${el.cicd.DEVOPS_EMAIL_LIST}\"",
-                               "git add ${projectInfo.id}.yml",
-                               "git commit -am \"writing el-CICD-config Project Definition File for ${projectInfo.id}\"",
+                               "git add ${args.projectId}.yml",
+                               "git commit -am \"writing el-CICD-config Project Definition File for ${args.projectId}\"",
                                "git push --set-upstream origin development"}
             """
         }
