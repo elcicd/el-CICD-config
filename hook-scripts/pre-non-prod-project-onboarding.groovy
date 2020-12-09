339251d66b2e15b4e69f9c05889c654637f10fa4
hippyod <hippyod@yahoo.com>
2020-12-09 12:57:46 -0600

    programming error

--- hook-scripts/pre-non-prod-project-onboarding.groovy
+++ hook-scripts/pre-non-prod-project-onboarding.groovy
@@ -13,12 +13,14 @@ def call(Map args) {
                 #git config user.email "${el.cicd.DEVOPS_EMAIL_LIST}"
                 #git commit -am 'writing el-CICD-config Project Definition File for ${projectInfo.id}'
                 ${sshAgentBash GITHUB_PRIVATE_KEY,
-                                    "git config --global user.name \"el-CICD\"",
-                                    "git config --global user.email \"${el.cicd.DEVOPS_EMAIL_LIST}\"",
-                                    "git add ${projectInfo.id}.yml",
-                                    "git commit -am \"writing el-CICD-config Project Definition File for ${projectInfo.id}\"",
-                                    "git push --set-upstream origin development"}
+                               "git config --global user.name \"el-CICD\"",
+                               "git config --global user.email \"${el.cicd.DEVOPS_EMAIL_LIST}\"",
+                               "git add ${projectInfo.id}.yml",
+                               "git commit -am \"writing el-CICD-config Project Definition File for ${projectInfo.id}\"",
+                               "git push --set-upstream origin development"}
             """
         }
     }
 }
+
+return this
