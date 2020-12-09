cb64228409f46191df8907d1318c57b78d5e4aa6
hippyod <hippyod@yahoo.com>
2020-12-09 14:31:57 -0600

    minor change

--- hook-scripts/pre-non-prod-project-onboarding.groovy
+++ hook-scripts/pre-non-prod-project-onboarding.groovy
@@ -3,22 +3,7 @@
  */
 
 def call(Map args) {
-    dir (el.cicd.PROJECT_DEFS_DIR) {
-        sh "cp ${args.projectId}.yml ${args.projectId}-test.yml"
-
-        withCredentials([sshUserPrivateKey(credentialsId: el.cicd.EL_CICD_PROJECT_INFO_REPOSITORY_READ_ONLY_GITHUB_PRIVATE_KEY_ID, keyFileVariable: 'GITHUB_PRIVATE_KEY')]) {
-            sh """
-                #git add ${el.cicd.PROJECT_DEFS_DIR}/${args.projectId}.yml
-                #git config user.name "el-CICD"
-                #git config user.email "${el.cicd.DEVOPS_EMAIL_LIST}"
-                #git commit -am 'writing el-CICD-config Project Definition File for ${args.projectId}'
-                ${sshAgentBash GITHUB_PRIVATE_KEY,
-                               "git add ${args.projectId}-test.yml",
-                               "git commit -am \"writing el-CICD-config Project Definition File for ${args.projectId}-test\"",
-                               "git push"}
-            """
-        }
-    }
+    pipelineUtils.echoBanner("WE'RE IN THE PRE SCRIPT!!!!!!")
 }
 
 return this
