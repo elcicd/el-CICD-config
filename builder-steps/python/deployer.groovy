34ad888519c7f1e53b7fde5adb23d847b1ef29c9
hippyod <hippyod@yahoo.com>
2021-03-31 17:17:57 -0500

    first pass adding library builds

---
+++ builder-steps/python/deployer.groovy
@@ -0,0 +1,18 @@
+/* 
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Utility method for deploying a library to an artifact repository
+ */
+
+def assemble(def projectInfo, def library) {
+    echo 'WARNING: library deployments not currently enabled for Python builds'
+
+    // if (library.type == 'EGG') {
+    //     sh 'python setup.py sdist upload -r local'
+    // }
+    // else {
+    //     sh 'python setup.py bdist_wheel upload -r local'
+    // }
+}
+
+return this
