caef1ede416ed95c641d71d6a595ed37ce7e7681
hippyod <hippyod@yahoo.com>
2022-01-19 23:26:59 -0600

    completing retooling Dockerfiles for rootless image building with podman; added sample imageScanner builder step

---
+++ builder-steps/imageScanner.groovy
@@ -0,0 +1,11 @@
+/* 
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Utility method for scanning images
+ */
+
+def scanImage(def projectInfo, def microService) {
+    pipelineUtils.echoBanner("Scan Image: ${microService}", '', 'EXAMPLE ONLY: replace sample script to scan images before pushing to registry')
+}
+
+return this
