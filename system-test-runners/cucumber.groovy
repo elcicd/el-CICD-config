c09ff6f4cc6327436a693f7403eeff6f6b335ec2
hippyod <hippyod@yahoo.com>
2022-03-17 21:29:44 -0500

    removed OKD 3.11 support files; fixed Jenkins builds; updated conf files; added placeholders pending implementation of system test runners

---
+++ system-test-runners/cucumber.groovy
@@ -0,0 +1,11 @@
+/* 
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Example script where system tests can be run...
+ */
+
+def runSystemTests(def projectInfo, def microServicesToTest) {
+    echo "[CUCUMBER RUNNER PLACEHOLDER]: Modify to run acceptance, integration, and/or smoke tests written for Cucumber..."
+}
+
+return this
---
+++ system-test-runners/spock.groovy
@@ -0,0 +1,11 @@
+/* 
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Example script where system tests can be run...
+ */
+
+def runSystemTests(def projectInfo, def microServicesToTest) {
+    echo "[SPOCK RUNNER PLACEHOLDER]: Modify to run acceptance, integration, and/or smoke tests written for Spock..."
+}
+
+return this
