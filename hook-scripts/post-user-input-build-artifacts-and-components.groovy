3dcd7e071e824dd8dafea0c6901522a7722536d9
hippyod <hippyod@yahoo.com>
2023-04-01 21:39:51 -0500

    first pass adding user input pre- and post- user input hook scripts

---
+++ hook-scripts/post-user-input-build-artifacts-and-components.groovy
@@ -0,0 +1,9 @@
+/*
+ * on-success-non-prod project test script
+ */
+
+def call(Map args) {
+    loggingUtils.echoBanner("RUNNING IN THE EXAMPLE BUILD ARTIFACTS AND COMPONENTS POST USER INPUT HOOK SCRIPT")
+}
+
+return this
