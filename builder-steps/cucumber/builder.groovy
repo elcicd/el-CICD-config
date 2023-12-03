d529aee3d6d4def91259a42c29f7fe368a2906af
hippyod <hippyod@yahoo.com>
2023-12-03 01:35:31 -0600

    updating tests

---
+++ builder-steps/cucumber/builder.groovy
@@ -0,0 +1,11 @@
+/* 
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Utility method for building Python components
+ */
+
+def build(def projectInfo, def component) {
+    echo 'CUCUMBER: BUILD EXECUTES AT RUNTIME'
+}
+
+return this
---
+++ builder-steps/cucumber/tester.groovy
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Utility method for running Python pytest based unit tests
+ */
+
+def test(def projectInfo, def component) {
+    echo 'CUCUMBER: TEST MODULE'
+}
+
+return this
