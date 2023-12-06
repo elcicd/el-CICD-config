deb9c87be6415fa456e8ea9afd43e05ad56cae49
hippyod <hippyod@yahoo.com>
2023-12-05 21:59:46 -0600

    adding linting build step for deployments; updating test config in project defs

---
+++ builder-steps/linter.groovy
@@ -0,0 +1,12 @@
+/* 
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Utility method for scanning images
+ */
+
+def lint(def projectInfo, def component) {
+    echo 'EXAMPLE DEPLOYMENT MANIFESTS LINTING COMPLETED (FAKE):'
+    echo '   Replace sample script to do actual linting of pre-deployment component deployment manifests'
+}
+
+return this
