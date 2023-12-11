24d9e3f1abc55415d13db4686714d80a3318e3f2
hippyod <hippyod@yahoo.com>
2023-12-11 00:02:27 -0600

    updating builds to use centos stream 9 minimal vs fedora; fixed a few build problems with R

--- jenkins/requirements.R
+++ jenkins/requirements.R
@@ -1,14 +1,14 @@
 #!/bin/Rscript
 
-cran.packages <- c('devtools', 'renv', 'lintr', 'jsonlite', 'xml2', 'covr', 'DT', 'testthat')
-install.opts <- c("--no-help", "--no-html", "--no-docs")
+cran.packages <- c('devtools', 'renv', 'lintr', 'jsonlite')
+install.opts <- c('--no-help', '--no-html', '--no-docs', '--no-docs','--no-multiarch')
 
 install.packages(
   cran.packages,
-  repos = "http://cran.us.r-project.org/",
+  repos = 'https://cloud.r-project.org/',
   dependencies = TRUE,
-  type = "source",
-  Ncpus = "4",
+  type = 'source',
+  Ncpus = '4',
   clean = TRUE,
   INSTALL_opts = install.opts
 )
